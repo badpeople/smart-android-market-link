ade4096305d86f0f45c28f1f2530302c143cb732
Paul <brower.paul@gmail.com>
2011-04-30 11:41:27 -0400

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartAndroidMarketLink::Application.config.secret_token = '0222bbd2e06f6b55f643d70d01079f1d538a42afbd7237c503a030e60f39c9cbbc7e6d0deea3d6c0316d6e8ea208516c774e4493b95b170e97d1c1d37309177e'
