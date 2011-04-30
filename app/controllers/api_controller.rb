bde635dce0895378fcbdd4525094d36e2ad0b8cb
Paul <brower.paul@gmail.com>
2011-04-30 13:33:22 -0400

    referrer stuff, analytics, contact

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,12 @@
 class ApiController < ApplicationController
   def r
-    package_name = params[:q]
+    package_name = params[:id]
     if !package_name.nil? then
+      path = "details?id=#{package_name}&referrer=#{params[:referrer]}"
       if request.headers['HTTP_USER_AGENT'].include?('Android')
-        redirect_to "market://details?id=#{package_name}"
+        redirect_to "market://"+path
       else
-        redirect_to "http://market.android.com/details?id=#{package_name}"
+        redirect_to "http://market.android.com/"+path
       end
     end
   end
